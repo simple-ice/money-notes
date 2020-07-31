28517907480afb45ee86fcb3250e8c88cf836aab
simple-ice <simplewu233@gmail.com>
2020-08-01 03:37:16 +0800

    创建三个页面

---
+++ src/views/Money.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Money组件</div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Money'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Statistics组件</div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Statistics'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
