7f1841db259de5bd961d615585e1fa6bc661a3e3
simple-ice <simplewu233@gmail.com>
2020-08-01 07:46:32 +0800

    引入svg 并且创建Icon组件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+    lintOnSave: false,
+    chainWebpack: config => {
+
+        const dir = path.resolve(__dirname, 'src/assets/icons')
+        // config.module.rule('svg').exclude.add(dir) //将其他路径下的svg排除
+        config.module
+            .rule('svg-sprite')
+            .test(/\.svg$/)
+            .include.add(dir).end() //只包含icons目录
+            .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+        config.module.rule('svg').exclude.add(dir) //将其他路径下的svg排除
+
+    }
 }
