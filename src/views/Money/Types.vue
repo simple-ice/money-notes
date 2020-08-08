749ed11200447510953504878deb8bdaed480296
simple-ice <simplewu233@gmail.com>
2020-08-08 15:07:32 +0800

    将Types组件的功能，使用JS实现

--- src/views/Money/Types.vue
+++ src/views/Money/Types.vue
@@ -1,13 +1,26 @@
 <template>
     <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
+        <li :class="type === '-' && 'selected' "  @click="selectType('-')">支出</li>
+        <li :class=" type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
 </template>
 
-<script lang="ts">
+<script>
     export default {
-        name: 'Types'
+        name: 'Types',
+        data() {
+            return {
+                type: '-'  // '-' ： 支出。 '+'：收入。
+            }
+        },
+        methods: {
+            selectType(type) {
+                if (type !== '-' && type !== '+'){
+                    throw new Error('type is unknown')
+                }
+                this.type = type;
+            }
+        }
     };
 </script>
 
@@ -31,7 +44,7 @@
                 bottom: 0;
                 left: 0;
                 width: 100%;
-                height: 3px;
+                height: 4px;
                 background: #333;
             }
         }
