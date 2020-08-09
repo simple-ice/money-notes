023b920cefb5e7685776f16e0539b184fb2d694a
simple-ice <simplewu233@gmail.com>
2020-08-09 08:21:59 +0800

    基本完成 Remark.vue 组件功能

--- src/views/Money/Remark.vue
+++ src/views/Money/Remark.vue
@@ -1,28 +1,34 @@
 <template>
     <label class="remark">
         <span class="name">备注</span>
-        <input type="text" placeholder="在这里添加备注">
+        <input v-model="value" type="text" placeholder="在这里添加备注"/>
     </label>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Remark'
-    };
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+
+    @Component
+    export default class Remark extends Vue {
+        value = '';
+    }
 </script>
 
 <style lang="scss" scoped>
-    .remark{
+    .remark {
         width: 100%;
         font-size: 14px;
         background: #f5f5f5;
         padding-left: 16px;
         display: flex;
         align-items: center;
-        .name{
+
+        .name {
             padding-right: 16px;
         }
-        > input{
+
+        > input {
             height: 73px;
             flex-grow: 1;
             padding-right: 16px;
