d3fccda634e9ae834f902abd1cbfcd95cc234de6
simple-ice <simplewu233@gmail.com>
2020-08-11 17:13:50 +0800

    将 tagListModel.ts 中 data 的类型 string 改为{id, name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,8 +1,13 @@
 const localStorageKeyName = 'tagList';
 
+type Tag = {
+    id: string;
+    name: string;
+}
+
 type TagListModel = {
-    data: string[];
-    getData: () => string[];
+    data: Tag[];
+    getData: () => Tag[];
     createData: (data: string) => 'success' | 'duplicated';// 联合类型
     saveData: () => void;
 }
@@ -13,10 +18,14 @@ const tagListModel: TagListModel = {
         return this.data;
     },
     createData(name) {
-        if (this.data.indexOf(name) >= 0) {
+        const names = this.data.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
             return 'duplicated'
         }
-        this.data.push(name);
+        this.data.push({
+            id: name,
+            name: name
+        });
         this.saveData();
         return 'success';
     },
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout>
         <ol class="tags">
-            <li v-for="tag in tags" :key="tag"><span>{{tag}}</span>
+            <li v-for="tag in tags" :key="tag.id"><span>{{tag.name}}</span>
                 <Icon name="right"/>
             </li>
         </ol>
