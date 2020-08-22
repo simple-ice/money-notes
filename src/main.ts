48b1527a44b94e7598800c8300e4196869039d72
simple-ice <simplewu233@gmail.com>
2020-08-22 15:43:07 +0800

    判断用户是否在移动端

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,22 @@ new Vue({
     store,
     render: h => h(App)
 }).$mount('#app');
+
+function IsPC() {
+    const userAgentInfo = navigator.userAgent;
+    const Agents = ["Android", "iPhone",
+        "SymbianOS", "Windows Phone",
+        "iPad", "iPod"];
+    let flag = true;
+    for (let v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+//true为PC端，false为手机端
+if (IsPC()){
+    window.alert('使用手机浏览可以获得最佳体验');
+}
