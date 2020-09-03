c73482b1aa065f90c8aafdcb887ce3666ed167e7
simple-ice <simplewu233@gmail.com>
2020-09-04 06:33:14 +0800

    完成 Chart 组件

---
+++ src/components/Chart.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="chart-wrap" ref="chartWrap">
+
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component, Prop, Watch} from 'vue-property-decorator';
+    import echarts, {EChartOption, ECharts} from 'echarts';
+
+    @Component
+    export default class Chart extends Vue {
+        @Prop(Object) options: EChartOption | undefined;
+        chart?: ECharts;
+
+        mounted() {
+            if (this.options === undefined) {
+                return console.error('options不能为空');
+            }
+            this.chart = echarts.init(this.$refs.chartWrap as HTMLDivElement, 'light');
+            this.chart.setOption(this.options);
+        }
+
+        @Watch('options', {immediate: true})
+        onChartChange(value: EChartOption) {
+            if (this.chart){
+                this.chart.setOption(value);
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
