c0dffc6470bba990b92a77df7344196c5af9510e
simple-ice <simplewu233@gmail.com>
2020-09-03 19:24:06 +0800

    初始化 Detail.vue 组件

---
+++ src/views/Detail.vue
@@ -0,0 +1,21 @@
+<template>
+    <Layout>
+        <div>
+            Detail 组件
+        </div>
+    </Layout>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+
+    @Component
+    export default class Detail extends Vue {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
